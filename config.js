70f267bb2421faf2250a9cfb857b1ce40309867e
frlegionaire <valentinvanhelle@hotmail.fr>
2025-04-08 22:13:04 +0200

    feat(config.js): add JWT_SECRET to configuration

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET manquant dans .env");
+}
 export default {
     PORT: process.env.PORT || 3000,
     API_PORT: process.env.API_PORT || 3001,
+    JWT_SECRET: process.env.JWT_SECRET,
 };
