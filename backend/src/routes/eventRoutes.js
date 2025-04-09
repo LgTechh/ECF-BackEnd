7fdd7c3d16d4026a68062eab1b1aaef3f2b62baf
frlegionaire <valentinvanhelle@hotmail.fr>
2025-04-09 20:34:29 +0200

    feat(eventRoutes.js): add validation middleware to event creation/update routes

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { getAllEvents, getEventById, createEvent, updateEvent, deleteEvent } from '../controllers/eventControllers.js';
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { eventValidationRules, idValidationRules, validate } from "../middlewares/validationMiddleware.js";
 
 const router = express.Router();
 
@@ -7,15 +9,15 @@ const router = express.Router();
 router.get('/', getAllEvents);
 
 // Route pour créer un événement
-router.post('/', createEvent);
+router.post('/', authMiddleware, eventValidationRules, validate, createEvent);
 
 // Route pour récupérer un événement par son ID
-router.get('/:id', getEventById);
+router.get('/:id',idValidationRules, validate, getEventById);
 
 // Route pour mettre à jour un événement
-router.put('/:id', updateEvent);
+router.put('/:id',authMiddleware, idValidationRules, eventValidationRules, validate, updateEvent);
 
 // Route pour supprimer un événement
-router.delete('/:id', deleteEvent);
+router.delete('/:id', authMiddleware, idValidationRules, validate, deleteEvent);
 
 export default router;
