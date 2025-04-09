7f44b38a82673125bdcad4d6a4de87fd72cb9f3f
frlegionaire <valentinvanhelle@hotmail.fr>
2025-04-09 20:33:25 +0200

    feat(authRoutes.js): add validation middleware for authentication routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { register, login, getProfile } from '../controllers/authController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { registerValidationRules, loginValidationRules, validate } from "../middlewares/validationMiddleware.js";
 
 const router = express.Router();
 
-router.post('/register', register);
+router.post('/register', registerValidationRules, validate, register);
 
-router.post('/login', login);
+router.post('/login', loginValidationRules, validate, login);
 
 // Route protégée pour récupérer le profil de l'utilisateur
 router.get('/profile', authMiddleware, getProfile);
